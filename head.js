0099e777678fd91491673d2dcee0f874061e2795
Rylan Baun <rylanjbaun@gmail.com>
2024-02-05 19:14:00 -0600

    Creating head function

---
+++ head.js
@@ -0,0 +1,13 @@
+const head = function(arr) {
+  return arr[0];
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(head([5,2,3]), (5));
