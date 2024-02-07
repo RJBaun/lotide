53e907c7e1f09d5ed19cf0e1136989915357bb09
Rylan Baun <rylanjbaun@gmail.com>
2024-02-06 21:50:29 -0600

    using correct functions and changing console logs

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,22 @@
-const assertArraysEqual = function(arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    console.log(`Assertion failed: ${arr1} and ${arr2} are not identical.`);
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        console.log(`Assertion Failed: ${arr1} and ${arr2} are not identical.`);
-      } else {
-        console.log(`Assertion Passed: ${arr1} and ${arr2} are identical.`);
-      }
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
     }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  const result = eqArrays(arr1, arr2);
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} and ${arr2} are identical!`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${arr1} and ${arr2} are not identical!`);
   }
 };
 
-assertArraysEqual(['Emma', 'is', 'cool'], ['Emma', 'is', 'not', 'cool']);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
