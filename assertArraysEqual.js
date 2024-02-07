78b06d663efc65806c8648cb3dc3f12b19ecc783
Rylan Baun <rylanjbaun@gmail.com>
2024-02-06 21:27:23 -0600

    Removing second function, console logging within function.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,15 @@
-const eqArrays = function(arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+    console.log(`Assertion failed: ${arr1} and ${arr2} are not identical.`);
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        console.log(`Assertion Failed: ${arr1} and ${arr2} are not identical.`);
+      } else {
+        console.log(`Assertion Passed: ${arr1} and ${arr2} are identical.`);
+      }
     }
   }
-  return true;
 };
 
-const assertArraysEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual(['Emma', 'is', 'cool'], ['Emma', 'is', 'not', 'cool']);
