1af2d6b3b3e3dddd7433d4e33eafd92a47de45a6
Rylan Baun <rylanjbaun@gmail.com>
2024-02-06 22:18:21 -0600

    Removing filter, replacing with a for loop.

--- without.js
+++ without.js
@@ -20,9 +20,27 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 const without = function(source, itemsToRemove) {
-  let removedArr = source.filter(function(item) {
-    return !itemsToRemove.includes(item);
-  });
+  let removedArr = [];
+  for (let i =0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      removedArr.push(source[i]);
+    }
+  }
   return removedArr;
 };
 
+console.log(without([1, 2, 3], [1, 2]));
+
+/*Implement the without function
+Good job on your without function! It correctly takes two arrays as arguments and returns a copy of the first array with any elements present in the second array removed.
+
+However, the requirement specifies not to use higher-order functions like filter, but in your without function, you used filter:
+
+let removedArr = source.filter(function(item) {
+  return !itemsToRemove.includes(item);
+});
+Instead of using filter, you can use a simple for loop to iterate over the source array and use includes to check if the current item is in itemsToRemove. If it's not, you can add it to a new array.
+
+Also, remember to include some tests using assertArraysEqual to confirm that without behaves correctly.
+
+Keep going! You're almost there.*/
