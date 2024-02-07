6fc0a79063d70284dc1793b7886745fa24a02d38
Rylan Baun <rylanjbaun@gmail.com>
2024-02-07 17:54:43 -0600

    correcting assertion tests and adding test cases.

--- letterPositions.js
+++ letterPositions.js
@@ -14,13 +14,28 @@ const letterPositions = function(sentence) {
   return results;
 };
 
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const assertArraysEqual = function(arr1, arr2) {
-  //const result = eqArrays(arr1, arr2);
-  if (arr1 === arr2) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  const result = eqArrays(arr1, arr2);
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} and ${arr2} are identical!`);
   } else {
-    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
+    console.log(`❌❌❌ Assertion Failed: ${arr1} and ${arr2} are not identical!`);
   }
 };
 
+assertArraysEqual(letterPositions("hello")['h'], [0]);
+assertArraysEqual(letterPositions("hello")['e'], [1]);
 assertArraysEqual(letterPositions("hello")['l'], [2, 3]);
+assertArraysEqual(letterPositions("hello")['o'], [4])
