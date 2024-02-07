01c9993f383bd48411670f7e77775be9abfe58e1
Rylan Baun <rylanjbaun@gmail.com>
2024-02-06 22:09:46 -0600

    Added without function

---
+++ without.js
@@ -0,0 +1,28 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  const result = eqArrays(arr1, arr2);
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} and ${arr2} are identical!`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${arr1} and ${arr2} are not identical!`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let removedArr = source.filter(function(item) {
+    return !itemsToRemove.includes(item);
+  });
+  return removedArr;
+};
+
