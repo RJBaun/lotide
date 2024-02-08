35c742cda6ab30590aa6007ee66fe4a0ba966fbc
Rylan Baun <rylanjbaun@gmail.com>
2024-02-07 18:58:44 -0600

    correcting where eqObjects returns true, checking type is array before pushing to eqArarys().

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -4,12 +4,14 @@ const eqObjects = function(object1, object2) {
   } else {
     for (const key in object1) {
       if (object1[key] !== object2[key]) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
+        if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+          if (!eqArrays(object1[key], object2[key])) {
+            return false;
+          }
         }
       }
-      return true;
     }
+    return true;
   }
 };
 
