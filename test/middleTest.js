c1ebac5aa7527c5c293a7ef5773e1c463cb1ab26
Rylan Baun <rylanjbaun@gmail.com>
2024-02-12 14:28:06 -0600

    removing manual test from test files

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,12 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(middle(['5']), []);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -12,14 +12,3 @@ describe("#tail", () => {
 });
 
 
-const result = tail(["hello", "from", "the", "future"]);
-const words = ['here', 'are', 'some', 'words'];
-const originalLength = words.length;
-tail(words);
-const testEmpty = [];
-
-
-assertEqual(result[0], "from");
-assertEqual(result.length, 3);
-assertEqual(words.length, originalLength);
-assertEqual(tail(testEmpty).length, 0);
