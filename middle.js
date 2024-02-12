a38b5da3884b3a5b7177bc1f9cc52a01202b3a62
Rylan Baun <rylanjbaun@gmail.com>
2024-02-12 10:41:06 -0600

    drying code by using modules

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  const result = eqArrays(arr1, arr2);
-  if (result) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} and ${arr2} are identical!`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${arr1} and ${arr2} are not identical!`);
-  }
-};
-
-
-
 const middle = function(array) {
   let middleArr = [];
   if (array.length <= 2) {
@@ -35,5 +12,4 @@ const middle = function(array) {
   }
 };
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
