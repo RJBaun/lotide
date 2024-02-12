4ca3a090fcc8b4fc4f9a1f1d553fac48e82e9bff
Rylan Baun <rylanjbaun@gmail.com>
2024-02-12 14:25:28 -0600

    adding unit tests for tail and middle functions

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,15 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(tail(['5']), []);
+  });
+});
 
 
 const result = tail(["hello", "from", "the", "future"]);
