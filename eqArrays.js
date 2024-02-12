976d8eae2f82d6ffa3699f492cea58a8ce2508c2
Rylan Baun <rylanjbaun@gmail.com>
2024-02-12 10:04:30 -0600

    refactoring and drying code with modules

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    if (Array.isArray(arr1[i])) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
       if (!eqArrays(arr1[i], arr2[i])) {
         return false;
       }
@@ -22,16 +14,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-
-
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);  // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-assertEqual(eqArrays([],[]), true);
-assertEqual(eqArrays([[2, [3]], [4]], [[2, [3]], [4]]), true); // => true
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
-  
+module.exports = eqArrays;
